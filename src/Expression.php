863440a2182e037b53bd294510c776dbbc5e4924
Sakinom <sakinom328@gmail.com>
2024-09-25 12:02:45 +0900

    chapter16終了時点のコード

--- src/Expression.php
+++ src/Expression.php
@@ -2,6 +2,7 @@
 
 interface Expression
 {
+  public function times(int $multiplier): Expression;
   public function plus(Expression $addend): Expression;
   public function reduce(Bank $bank, string $to): Money;
 }
